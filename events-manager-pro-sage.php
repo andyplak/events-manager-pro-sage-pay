f3a11ae77fa3b95b66efa84a9b6579163d952c8a
Andy Place <info@andyplace.co.uk>
2014-10-27 21:32:50 +0000

    NOOP tabs and indentation

--- events-manager-pro-sage.php
+++ events-manager-pro-sage.php
@@ -49,7 +49,7 @@ class EM_Pro_Sage {
 		//Set when to run the plugin : after EM is loaded.
 		add_action( 'plugins_loaded', array(&$this,'init'), 100 );
 	}
-	
+
 	function init() {
 		//add-ons
 		include('add-ons/gateways/gateway.sagepay.form.php');
